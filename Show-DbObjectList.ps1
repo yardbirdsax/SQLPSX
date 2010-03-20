af77b36c5f1e45711d03471eff212c089c908714
smurawski <smurawski@localhost>
2010-03-20 15:55:35 +0000

    Added Relations, Operations, and Keys. Added Vertical Scrollbar.

--- Show-DbObjectList.ps1
+++ Show-DbObjectList.ps1
@@ -1,29 +1,51 @@
 New-Window {
 @'
-<Canvas>
-    <Canvas.Resources>
+<ScrollViewer>
+    <ScrollViewer.Resources>
             <DataTemplate 
         		x:Key = "ColumnTemplate">
         	  <TreeViewItem Header="{Binding Column}" />
+        	</DataTemplate>  
+            <DataTemplate 
+        		x:Key = "KeysTemplate">
+        	  <TreeViewItem Header="{Binding COLUMN_NAME}" />
         	</DataTemplate>        	
-        	<HierarchicalDataTemplate 
-        		x:Key = "TableTemplate"
-        		ItemsSource="{Binding Table2Column}" 
-        		ItemTemplate="{StaticResource ColumnTemplate}" >
-        		<TextBlock Text="{Binding Table}" /> 
-        	</HierarchicalDataTemplate>        	
+        	<DataTemplate 
+        		x:Key = "OperationsTemplate">
+        	  <TreeViewItem Header="{Binding operation}" />
+        	</DataTemplate>  
+            <DataTemplate 
+        		x:Key = "RelationsTemplate">
+        	  <TreeViewItem Header="{Binding Relation}" />
+        	</DataTemplate>
+            <DataTemplate 
+        		x:Key = "TableTemplate" >
+        		<TreeViewItem Header="{Binding Table}" >
+                    <TreeViewItem Header="Columns"
+                        ItemsSource="{Binding Table2Column}" 
+                        ItemTemplate="{StaticResource ColumnTemplate}" />
+                    <TreeViewItem Header="Keys"
+                        ItemsSource="{Binding Table2Keys}" 
+                        ItemTemplate="{StaticResource KeysTemplate}" />
+                    <TreeViewItem Header="Relations"
+                        ItemsSource="{Binding Table2Relations}" 
+                        ItemTemplate="{StaticResource RelationsTemplate}" />
+                    <TreeViewItem Header="Operations"
+                        ItemsSource="{Binding Table2Operations}" 
+                        ItemTemplate="{StaticResource OperationsTemplate}" />
+                </TreeViewItem>             
+        	</DataTemplate>        	
         	<HierarchicalDataTemplate 
         		x:Key = "DatabaseTemplate"
         		ItemsSource="{Binding Database2Table}" 
         		ItemTemplate="{StaticResource TableTemplate}" >
-        		<TextBlock Text="{Binding Database}" />  
+        		<TreeViewItem Header="{Binding Database}" />  
         	 </HierarchicalDataTemplate>
-    </Canvas.Resources>
+    </ScrollViewer.Resources>
     <TreeView 
     	ItemsSource="{Binding Database}"
-    	ItemTemplate="{StaticResource DatabaseTemplate}"
-    	SelectedValuePath="Database">
+    	ItemTemplate="{StaticResource DatabaseTemplate}" >
     </TreeView>
-</Canvas>
+</ScrollViewer>
 '@
-} -DataContext $ds -Width 300 -Height 300 -WindowStartupLocation CenterScreen -show 
+} -DataContext $ds -Height 300 -Width 300 -WindowStartupLocation CenterScreen  -show
